4abb5ee9d9fb17793841f4e0676d0687b68155c7
calebewerneckcouto <calebewerneck@hotmail.com>
2025-10-24 19:48:05 -0300

    jacoco e implementação

--- src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
+++ src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
@@ -3,11 +3,11 @@ package br.com.alura.adopet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AdopetApiApplicationTests.class)
 class AdopetApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
